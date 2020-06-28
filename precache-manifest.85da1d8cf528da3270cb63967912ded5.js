bdf6a6775f62c92e4378be35bf86e5c10f2cdbcb
upgradethecompany <wilson.tibia.valentin@gmail.com>
2020-06-28 18:54:19 -0300

    Updates

---
+++ precache-manifest.85da1d8cf528da3270cb63967912ded5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ee9bd22d0400e62bb045f2dbf31d8a",
+    "url": "/wcarugatti/react-challenge/index.html"
+  },
+  {
+    "revision": "77103a29f4f806d0acb2",
+    "url": "/wcarugatti/react-challenge/static/css/main.3a6a09d8.chunk.css"
+  },
+  {
+    "revision": "889e2e7e266092931997",
+    "url": "/wcarugatti/react-challenge/static/js/2.27e04c76.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/wcarugatti/react-challenge/static/js/2.27e04c76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77103a29f4f806d0acb2",
+    "url": "/wcarugatti/react-challenge/static/js/main.74eb9a1a.chunk.js"
+  },
+  {
+    "revision": "f8cd6d29c0067cb4f01a",
+    "url": "/wcarugatti/react-challenge/static/js/runtime-main.c7c44d7c.js"
+  }
+]);
